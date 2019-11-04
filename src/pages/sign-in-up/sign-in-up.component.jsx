f928659042a07b4e6b54b1a9b04878254d1d4449
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-11-04 10:06:26 -0300

    sign up

--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './sign-in-up.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInUp = () => (
     <div className='sign-in-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
