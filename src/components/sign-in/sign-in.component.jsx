74104b6626ed400d596371266cca418ac960d278
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-11-04 17:36:45 -0300

    implementing redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -24,7 +24,7 @@ class SignIn extends React.Component {
 
     try {
       await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: ''});
+      this.setState({ email: '', password: '' });
     } catch (error) {
       console.error(error);
     }
