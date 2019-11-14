48efa31dc1a6511f1315e28a3ca9fbd937cee9e4
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-11-14 14:44:17 -0300

    cart add item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,24 +1,34 @@
 import {
-    CartActionTypes
-} from './cart-types';
+  CartActionTypes
+} from "./cart-types";
+
+import {
+  addItemToCart
+} from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, {
-    type,
-    action
+  type,
+  payload
 }) => {
-    switch (type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            };
-        default:
-            return state;
-    }
-}
+  switch (type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, payload)
+      }
+      default:
+        return state;
+  }
+};
 
 export default cartReducer;
